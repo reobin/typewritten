c2b8e0be8b098d4d1ca21f4838a494c7f52fc136
Robin Gagnon <me@reobin.dev>
2020-08-04 16:40:14 -0400

    feat: Add color mappings option
    * Lets the user change the actual value of one color
      Ex.:(i.e.: magenta -> blue)

--- lib/colors.zsh
+++ lib/colors.zsh
@@ -24,17 +24,47 @@ declare -A colors=(
   ["git_status_stash"]="yellow"
 )
 
+declare -A color_mappings=(
+  ["default"]="default"
+  ["white"]="white"
+  ["black"]="black"
+  ["magenta"]="magenta"
+  ["blue"]="blue"
+  ["red"]="red"
+  ["yellow"]="yellow"
+  ["green"]="green"
+  ["cyan"]="cyan"
+)
+
 _setup_custom_colors() {
+  if [[ $TYPEWRITTEN_COLOR_MAPPINGS =~ ^[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+(\;[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+)*$ ]]; then
+    values=($(echo $TYPEWRITTEN_COLOR_MAPPINGS | tr ";" "\n"))
+    for value in $values; do
+      value_definition=($(echo $value | tr ":" "\n"))
+      color_mappings[$value_definition[1]]=$value_definition[2]
+    done
+  elif [[ ! -z $TYPEWRITTEN_COLOR_MAPPINGS ]]; then
+    echo "$TYPEWRITTEN_COLOR_MAPPINGS is not formatted correctly.
+Format it like so: \"value:#009090;value:red\", etc."
+  fi
+
   if [[ $TYPEWRITTEN_COLORS =~ ^[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+(\;[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+)*$ ]]; then
-    custom_colors=($(echo $TYPEWRITTEN_COLORS | tr ";" "\n"))
-    for value in "${custom_colors[@]}"; do
-      color_definition=($(echo $value | tr ":" "\n"))
-      colors[$color_definition[1]]="$color_definition[2]"
+    values=($(echo $TYPEWRITTEN_COLORS | tr ";" "\n"))
+    for value in $values; do
+      value_definition=($(echo $value | tr ":" "\n"))
+      colors[$value_definition[1]]=$value_definition[2]
     done
   elif [[ ! -z $TYPEWRITTEN_COLORS ]]; then
-    echo "TYPEWRITTEN_COLORS is not formatted correctly.
-Format it like so: \"prompt:red;symbol:#008080;user:345\", etc."
+    echo "$TYPEWRITTEN_COLORS is not formatted correctly.
+Format it like so: \"value:#009090;value:red\", etc."
   fi
+
+  # apply mapped colors
+  for key value in ${(kv)colors}; do
+    if [ ! -z $color_mappings[$value] ]; then
+      colors[$key]=$color_mappings[$value]
+    fi
+  done
 }
 
 _setup_custom_colors

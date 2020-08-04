2c74ed46129aeb5cb4dce72f6d13aae568a2fde3
Robin Gagnon <me@reobin.dev>
2020-08-04 17:27:24 -0400

    fix: Hide arrow when not in git repo

--- typewritten.zsh
+++ typewritten.zsh
@@ -52,9 +52,9 @@ _redraw() {
   _env_prompt="$_virtualenv$_prompt"
 
   _layout="$TYPEWRITTEN_PROMPT_LAYOUT"
-  _git_info="%F{$_git_branch_color}$prompt_data[_git_branch]$prompt_data[_git_status]"
+  _git_info="$prompt_data[_git_branch]$prompt_data[_git_status]"
   if [ "$_layout" = "half_pure" ]; then
-    PROMPT="$BREAK_LINE$_git_info$BREAK_LINE$_env_prompt"
+    PROMPT="$BREAK_LINE%F{$_git_branch_color}$_git_info$BREAK_LINE$_env_prompt"
     RPROMPT="$_right_prompt_prefix%F{$_current_directory_color}$prompt_data[_git_home]$_current_directory"
   else
     local _git_arrow_info=""

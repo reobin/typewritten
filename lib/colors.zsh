2911e61e3bad05858bf5fceec1d28c2345cb4bea
Robin Gagnon <me@reobin.dev>
2020-08-04 17:14:15 -0400

    feat: Use semantic names for color mappings

--- lib/colors.zsh
+++ lib/colors.zsh
@@ -1,70 +1,72 @@
-declare -A colors=(
-  ["symbol"]="blue"
-  ["symbol_error"]="red"
-  ["error_code"]="red"
-  ["current_directory"]="magenta"
-  ["right_prompt_prefix"]="default"
-  ["host"]="yellow"
-  ["host_user_connector"]="default"
-  ["user"]="yellow"
-  ["prompt"]="default"
-  ["virtualenv"]="default"
-  ["git_branch"]="magenta"
-  ["arrow"]="default"
-  ["git_rebasing"]="magenta"
-  ["git_status_staged"]="green"
-  ["git_status_new"]="blue"
-  ["git_status_modified"]="yellow"
-  ["git_status_renamed"]="cyan"
-  ["git_status_deleted"]="red"
-  ["git_status_unmerged"]="default"
-  ["git_status_diverged"]="blue"
-  ["git_status_ahead"]="blue"
-  ["git_status_behind"]="blue"
-  ["git_status_stash"]="yellow"
-)
-
 declare -A color_mappings=(
-  ["default"]="default"
-  ["white"]="white"
-  ["black"]="black"
-  ["magenta"]="magenta"
-  ["blue"]="blue"
-  ["red"]="red"
-  ["yellow"]="yellow"
-  ["green"]="green"
-  ["cyan"]="cyan"
+  ["foreground"]="default"
+
+  ["primary"]="magenta"
+  ["secondary"]="blue"
+
+  ["accent"]="default"
+
+  ["info_positive"]="green"
+  ["info_negative"]="red"
+
+  ["info_neutral_2"]="blue"
+  ["info_neutral_1"]="yellow"
+
+  ["info_special"]="cyan"
 )
 
-_setup_custom_colors() {
-  if [[ $TYPEWRITTEN_COLOR_MAPPINGS =~ ^[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+(\;[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+)*$ ]]; then
-    values=($(echo $TYPEWRITTEN_COLOR_MAPPINGS | tr ";" "\n"))
-    for value in $values; do
-      value_definition=($(echo $value | tr ":" "\n"))
-      color_mappings[$value_definition[1]]=$value_definition[2]
-    done
-  elif [[ ! -z $TYPEWRITTEN_COLOR_MAPPINGS ]]; then
-    echo "$TYPEWRITTEN_COLOR_MAPPINGS is not formatted correctly.
-Format it like so: \"value:#009090;value:red\", etc."
-  fi
-
-  if [[ $TYPEWRITTEN_COLORS =~ ^[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+(\;[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+)*$ ]]; then
-    values=($(echo $TYPEWRITTEN_COLORS | tr ";" "\n"))
-    for value in $values; do
-      value_definition=($(echo $value | tr ":" "\n"))
-      colors[$value_definition[1]]=$value_definition[2]
-    done
-  elif [[ ! -z $TYPEWRITTEN_COLORS ]]; then
-    echo "$TYPEWRITTEN_COLORS is not formatted correctly.
-Format it like so: \"value:#009090;value:red\", etc."
-  fi
-
-  # apply mapped colors
-  for key value in ${(kv)colors}; do
-    if [ ! -z $color_mappings[$value] ]; then
-      colors[$key]=$color_mappings[$value]
-    fi
+if [[ $TYPEWRITTEN_COLOR_MAPPINGS =~ ^[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+(\;[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+)*$ ]]; then
+  values=($(echo $TYPEWRITTEN_COLOR_MAPPINGS | tr ";" "\n"))
+  for value in $values; do
+    value_definition=($(echo $value | tr ":" "\n"))
+    color_mappings[$value_definition[1]]=$value_definition[2]
   done
-}
+elif [[ ! -z $TYPEWRITTEN_COLOR_MAPPINGS ]]; then
+  echo "$TYPEWRITTEN_COLOR_MAPPINGS is not formatted correctly.
+Format it like so: \"value:#009090;value:red\", etc."
+fi
 
-_setup_custom_colors
+declare -A colors=(
+  ["prompt"]=$color_mappings[foreground]
+
+  ["current_directory"]=$color_mappings[primary]
+  ["git_branch"]=$color_mappings[primary]
+
+  ["symbol"]=$color_mappings[secondary]
+  ["git_status_new"]=$color_mappings[secondary]
+
+  ["arrow"]=$color_mappings[accent]
+  ["host_user_connector"]=$color_mappings[accent]
+  ["right_prompt_prefix"]=$color_mappings[accent]
+
+  ["symbol_error"]=$color_mappings[info_negative]
+  ["error_code"]=$color_mappings[info_negative]
+  ["git_status_deleted"]=$color_mappings[info_negative]
+
+  ["git_status_staged"]=$color_mappings[info_positive]
+
+  ["host"]=$color_mappings[info_neutral_1]
+  ["user"]=$color_mappings[info_neutral_1]
+  ["virtualenv"]=$color_mappings[info_neutral_1]
+  ["git_status_stash"]=$color_mappings[info_neutral_1]
+  ["git_status_modified"]=$color_mappings[info_neutral_1]
+
+  ["git_status_diverged"]=$color_mappings[info_neutral_2]
+  ["git_status_ahead"]=$color_mappings[info_neutral_2]
+  ["git_status_behind"]=$color_mappings[info_neutral_2]
+  ["git_rebasing"]=$color_mappings[info_neutral_2]
+
+  ["git_status_renamed"]=$color_mappings[info_special]
+  ["git_status_unmerged"]=$color_mappings[info_special]
+)
+
+if [[ $TYPEWRITTEN_COLORS =~ ^[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+(\;[#_0-9a-zA-Z]+:[#_0-9a-zA-Z]+)*$ ]]; then
+  values=($(echo $TYPEWRITTEN_COLORS | tr ";" "\n"))
+  for value in $values; do
+    value_definition=($(echo $value | tr ":" "\n"))
+    colors[$value_definition[1]]=$value_definition[2]
+  done
+elif [[ ! -z $TYPEWRITTEN_COLORS ]]; then
+  echo "$TYPEWRITTEN_COLORS is not formatted correctly.
+Format it like so: \"value:#009090;value:red\", etc."
+fi

8559a53f551a5a70789b7c96b95a128e8e0b794f
Robin Gagnon <me@reobin.dev>
2020-08-24 10:36:14 -0400

    fix: Give all variables a typewritten prefix
    * Fixes conflicts of variable names between typewritten and other actors
    of the zsh prompt.

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,96 +1,96 @@
-_git_home() {
-  local current_directory="$1"
-  local git_toplevel="$2"
-  local git_home=""
-  if [ "$git_toplevel" != "" -a "$git_toplevel" != "$current_directory" ]; then
-    local repo_name=`basename $git_toplevel`
-    git_home="$repo_name"
+tw_git_home() {
+  local tw_current_directory="$1"
+  local tw_git_toplevel="$2"
+  local tw_git_home=""
+  if [ "$tw_git_toplevel" != "" -a "$tw_git_toplevel" != "$tw_current_directory" ]; then
+    local tw_repo_name=`basename $tw_git_toplevel`
+    tw_git_home="$tw_repo_name"
   fi;
-  if [ "$git_home" != "" ]; then
+  if [ "$tw_git_home" != "" ]; then
     # check how many directories are between
-    local current_nesting="${current_directory//[^\/]}"
-    local repo_nesting="${git_toplevel//[^\/]}"
-    local diff=`expr ${#current_nesting} - ${#repo_nesting}`
-    if [ $diff -eq 1 ]; then
-      echo "$git_home/"
+    local tw_current_nesting="${tw_current_directory//[^\/]}"
+    local tw_repo_nesting="${tw_git_toplevel//[^\/]}"
+    local tw_diff=`expr ${#tw_current_nesting} - ${#tw_repo_nesting}`
+    if [ $tw_diff -eq 1 ]; then
+      echo "$tw_git_home/"
     else
-      echo "$git_home/.../"
+      echo "$tw_git_home/.../"
     fi;
   else
     echo ""
   fi;
 }
 
-_git_branch() {
+tw_git_branch() {
   echo "$(command git symbolic-ref --short -q HEAD)"
 }
 
-_git_status() {
-  local STATUS=$(command git status --porcelain -b 2> /dev/null)
+tw_git_status() {
+  local tw_git_status_response=$(command git status --porcelain -b 2> /dev/null)
 
-  local git_status=""
+  local tw_git_status_display=""
 
   # rebasing
-  if $(command grep "^## HEAD (no branch)" <<< "$STATUS" &> /dev/null); then
-    git_status="%F{$colors[git_rebasing]}rebasing"
+  if $(command grep "^## HEAD (no branch)" <<< "$tw_git_status_response" &> /dev/null); then
+    tw_git_status_display="%F{$tw_colors[git_rebasing]}rebasing"
   else
     # staged changes
-    if $(command grep "^A  " <<< "$STATUS" &> /dev/null) ||
-      $(command grep "^M  " <<< "$STATUS" &> /dev/null) ||
-      $(command grep "^D  " <<< "$STATUS" &> /dev/null); then
-          git_status+=" %F{$colors[git_status_staged]}+"
+    if $(command grep "^A  " <<< "$tw_git_status_response" &> /dev/null) ||
+      $(command grep "^M  " <<< "$tw_git_status_response" &> /dev/null) ||
+      $(command grep "^D  " <<< "$tw_git_status_response" &> /dev/null); then
+          tw_git_status_display+=" %F{$tw_colors[git_status_staged]}+"
     fi
 
     # new file
-    if $(command grep "?? " <<< "$STATUS" &> /dev/null); then
-      git_status+=" %F{$colors[git_status_new]}?"
+    if $(command grep "?? " <<< "$tw_git_status_response" &> /dev/null); then
+      tw_git_status_display+=" %F{$tw_colors[git_status_new]}?"
     fi
 
     # modified
-    if $(command grep "^ M " <<< "$STATUS" &> /dev/null); then
-      git_status+=" %F{$colors[git_status_modified]}!"
+    if $(command grep "^ M " <<< "$tw_git_status_response" &> /dev/null); then
+      tw_git_status_display+=" %F{$tw_colors[git_status_modified]}!"
     fi
 
     # renamed
-    if $(command grep "^R  " <<< "$STATUS" &> /dev/null); then
-      git_status+=" %F{$colors[git_status_renamed]}»"
+    if $(command grep "^R  " <<< "$tw_git_status_response" &> /dev/null); then
+      tw_git_status_display+=" %F{$tw_colors[git_status_renamed]}»"
     fi
 
     # deleted
-    if $(command grep "^ D " <<< "$STATUS" &> /dev/null); then
-      git_status+=" %F{$colors[git_status_deleted]}—"
+    if $(command grep "^ D " <<< "$tw_git_status_response" &> /dev/null); then
+      tw_git_status_display+=" %F{$tw_colors[git_status_deleted]}—"
     fi
 
     # unmerged
-    if $(command grep "^UU " <<< "$STATUS" &> /dev/null); then
-      git_status+=" %F{$colors[git_status_unmerged]}#"
+    if $(command grep "^UU " <<< "$tw_git_status_response" &> /dev/null); then
+      tw_git_status_display+=" %F{$tw_colors[git_status_unmerged]}#"
     fi
 
-    local is_ahead=false
-    if $(command grep "^## .*ahead" <<< "$STATUS" &> /dev/null); then
-      is_ahead=true
+    local tw_is_ahead=false
+    if $(command grep "^## .*ahead" <<< "$tw_git_status_response" &> /dev/null); then
+      tw_is_ahead=true
     fi
 
-    local is_behind=false
-    if $(command grep "^## .*behind" <<< "$STATUS" &> /dev/null); then
-      is_behind=true
+    local tw_is_behind=false
+    if $(command grep "^## .*behind" <<< "$tw_git_status_response" &> /dev/null); then
+      tw_is_behind=true
     fi
 
-    if [[ "$is_ahead" == true && "$is_behind" == true ]]; then
+    if [[ "$tw_is_ahead" == true && "$tw_is_behind" == true ]]; then
       # diverged
-      git_status+=" %F{$colors[git_status_diverged]}~"
-    elif [[ "$is_ahead" == true ]]; then
-      git_status+=" %F{$colors[git_status_ahead]}|•"
-    elif [[ "$is_behind" == true ]]; then
-      git_status+=" %F{$colors[git_status_behind]}•|"
+      tw_git_status_display+=" %F{$tw_colors[git_status_diverged]}~"
+    elif [[ "$tw_is_ahead" == true ]]; then
+      tw_git_status_display+=" %F{$tw_colors[git_status_ahead]}|•"
+    elif [[ "$tw_is_behind" == true ]]; then
+      tw_git_status_display+=" %F{$tw_colors[git_status_behind]}•|"
     fi
 
     # stashed
-    local stash_count=`git stash list | wc -l`
-    if [[ $stash_count -gt 0 ]]; then
-      git_status+=" %F{$colors[git_status_stash]}$"
+    local tw_stash_count=`git stash list | wc -l`
+    if [[ $tw_stash_count -gt 0 ]]; then
+      tw_git_status_display+=" %F{$tw_colors[git_status_stash]}$"
     fi
   fi
 
-  echo "$git_status"
+  echo "$tw_git_status_display"
 }

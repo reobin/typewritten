cc7af51599a160dd98122a24da2648ce3deb3bdf
J.B.C <57237705+juanCortelezzi@users.noreply.github.com>
2021-06-29 17:49:13 -0300

    fix(install): Improve npm install/uninstall scripts (#112)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,45 +1,137 @@
-SOURCE="$PWD"
-DEST="/usr/local/share/zsh/site-functions"
+#!/usr/bin/env zsh
+#
+# This script is a clone/fork of the spaceship-prompt install script and it was
+# changed in order to install the typewritten prompt instead.
+#
+# Original author: Denys Dovhan, denysdovhan.com
+# From: https://github.com/spaceship-prompt/spaceship-prompt
 
-SOURCE_TYPEWRITTEN="$SOURCE/typewritten.zsh"
-DEST_TYPEWRITTEN="$DEST/prompt_typewritten_setup"
+# ------------------------------------------------------------------------------
+# Colors
+# Set color variables for colorful output
+# ------------------------------------------------------------------------------
 
-SOURCE_ASYNC="$SOURCE/async.zsh"
-DEST_ASYNC="$DEST/async"
-
-ZSHRC="$HOME/.zshrc"
-
-echo "Installing typewritten...\n"
-
-mkdir -p "$DEST"
-
-link() {
-  local source="$1"
-  local dest="$2"
-  if [ -f "$dest" ]; then
-    echo "Unlinking $dest"
-    rm "$dest"
-  fi
-  echo "Linking $source -> $dest\n"
-  ln -sf "$source" "$dest"
-}
-
-link "$SOURCE_TYPEWRITTEN" "$DEST_TYPEWRITTEN"
-link "$SOURCE_ASYNC" "$DEST_ASYNC"
-
-echo "Setting up zsh prompt..."
-if sed "s/#.*//" "$ZSHRC" | grep -q "prompt typewritten"; then
-  echo "typewritten is already present in .zshrc!\n"
-  echo "Reload zsh."
-  exit
+# If we have tput, let's set colors
+if [[ ! -z $(which tput 2> /dev/null) ]]; then
+  reset=$(tput sgr0)
+  bold=$(tput bold)
+  red=$(tput setaf 1)
+  green=$(tput setaf 2)
+  yellow=$(tput setaf 3)
+  blue=$(tput setaf 4)
+  magenta=$(tput setaf 5)
+  cyan=$(tput setaf 6)
 fi
 
-config="\n# Set typewritten ZSH as a prompt"
-config+="\nautoload -U promptinit; promptinit"
-config+="\nprompt typewritten"
-echo "$config" >> "$ZSHRC"
+# ------------------------------------------------------------------------------
+# VARIABLES
+# Paths to important resources
+# ------------------------------------------------------------------------------
 
-echo "Done.\n"
-echo "Reload zsh.\n"
+ZSHRC="${ZDOTDIR:-$HOME}/.zshrc"
+REPO='https://github.com/reobin/typewritten.git'
 
-exit
+SOURCE="$PWD/typewritten.zsh"
+ASYNC_SOURCE="$PWD/async.zsh"
+USER_SOURCE="${ZDOTDIR:-$HOME}/.typewritten-prompt"
+
+DEST='/usr/local/share/zsh/site-functions'
+USER_DEST="${ZDOTDIR:-$HOME}/.zfunctions"
+
+# ------------------------------------------------------------------------------
+# HELPERS
+# Useful functions for common tasks
+# ------------------------------------------------------------------------------
+
+# Paint text in specific color with reset
+# USAGE:
+#   paint <color> [text...]
+paint() {
+  local color=$1 rest=${@:2}
+  echo "$color$rest$reset"
+}
+
+# Aliases for common used colors
+# Colon at the end is required: https://askubuntu.com/a/521942
+# USAGE:
+#   info|warn|error|success|code [...text]
+info()    { paint "$cyan"   "typewritten: $@" ; }
+warn()    { paint "$yellow" "typewritten: $@" ; }
+error()   { paint "$red"    "typewritten: $@" ; }
+success() { paint "$green"  "typewritten: $@" ; }
+code()    { paint "$bold"   "typewritten: $@" ; }
+
+# Append text in .zshrc
+# USAGE:
+#   append_zshrc [text...]
+append_zshrc() {
+  info "These lines will be added to your \"${ZDOTDIR:-$HOME}/.zshrc\" file:"
+  code "$@"
+  echo "$@" >> "${ZDOTDIR:-$HOME}/.zshrc"
+}
+
+# ------------------------------------------------------------------------------
+# MAIN
+# Checkings and installing process
+# ------------------------------------------------------------------------------
+
+main() {
+  # How we install typewritten:
+  #   1. Install via NPM
+  #   2. Install via curl or wget
+  if [[ ! -f "$SOURCE" ]]; then
+    warn "typewritten is not present in current directory"
+    # Clone repo into the ${ZDOTDIR:-$HOME}/.typewritten-prompt and change SOURCE
+    git clone "$REPO" "$USER_SOURCE"
+    SOURCE="$USER_SOURCE/typewritten.zsh"
+    ASYNC_SOURCE="$USER_SOURCE/async.zsh"
+  else
+    info "typewritten is present in current directory"
+  fi
+
+  # If we can't symlink to the site-functions, then try to use .zfunctions instead
+  if [[ ! -w "$DEST" ]]; then
+    error "Failed to symlink $SOURCE to $DEST."
+    error "Failed to symlink $ASYNC_SOURCE to $DEST."
+
+    # Use $USER_DEST instead
+    DEST="$USER_DEST"
+
+    info "Adding $DEST to fpath..."
+    echo 'fpath=($fpath "'"$DEST"'")' >> "$ZSHRC"
+
+    info "Trying to symlink $SOURCE to $DEST"
+    info "Trying to symlink $ASYNC_SOURCE to $DEST"
+  fi
+
+  # Link prompt entry point to fpath
+  info "Linking $SOURCE to $DEST/prompt_typewritten_setup..."
+  info "Linking $ASYNC_SOURCE to $DEST/async..."
+  mkdir -p "$DEST"
+  ln -sf "$SOURCE" "$DEST/prompt_typewritten_setup"
+  ln -sf "$ASYNC_SOURCE" "$DEST/async"
+
+  # If 'prompt typewritten' is already present in .zshrc, then skip
+  if sed 's/#.*//' "$ZSHRC" | grep -q "prompt typewritten"; then
+    warn "typewritten is already present in .zshrc!"
+    exit
+  fi
+
+  # Enabling statements for .zshrc
+  msg="\n# Set typewritten ZSH as a prompt"
+  msg+="\nautoload -U promptinit; promptinit"
+  msg+="\nprompt typewritten"
+
+  # Check if appending was successful and perform corresponding actions
+  if append_zshrc "$msg"; then
+    success "Done! Please, reload your terminal."
+    echo
+  else
+    error "Cannot automatically insert prompt init commands."
+    error "Please insert these line into your \"${ZDOTDIR:-$HOME}/.zshrc\" file:"
+    code "$msg"
+    exit 1
+  fi
+}
+
+main "$@"
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,10 +1,131 @@
-DEST="/usr/local/share/zsh/site-functions"
-DEST_TYPEWRITTEN="$DEST/prompt_typewritten_setup"
-DEST_ASYNC="$DEST/async"
+#!/usr/bin/env zsh
 
-echo "Unlinking zsh prompt files...\n"
-unlink "$DEST_TYPEWRITTEN"
-unlink "$DEST_ASYNC"
+# This script is a clone/fork of the spaceship-prompt uninstall script and it was
+# changed in order to install the typewritten prompt instead.
+#
+# Original author: Denys Dovhan, denysdovhan.com
+# From: https://github.com/spaceship-prompt/spaceship-prompt
 
-echo "Done.\n"
-echo "Don't forget to remove typewritten config lines in ~/.zshrc"
+# ------------------------------------------------------------------------------
+# Colors
+# Set color variables for colorful output
+# ------------------------------------------------------------------------------
+
+# If we have tput, let's set colors
+if [[ ! -z $(which tput 2> /dev/null) ]]; then
+  reset=$(tput sgr0)
+  bold=$(tput bold)
+  red=$(tput setaf 1)
+  green=$(tput setaf 2)
+  yellow=$(tput setaf 3)
+  blue=$(tput setaf 4)
+  magenta=$(tput setaf 5)
+  cyan=$(tput setaf 6)
+fi
+
+# ------------------------------------------------------------------------------
+# VARIABLES
+# Paths to important resources
+# ------------------------------------------------------------------------------
+
+ZSHRC="${ZDOTDIR:-$HOME}/.zshrc"
+USER_SOURCE="${ZDOTDIR:-$HOME}/.typewritten-prompt"
+
+PROMPT_SETUP="prompt_typewritten_setup"
+PROMPT_ASYNC="async"
+
+GLOBAL_DEST_SETUP="/usr/local/share/zsh/site-functions/$PROMPT_SETUP"
+USER_DEST_SETUP="${ZDOTDIR:-$HOME}/.zfunctions/$PROMPT_SETUP"
+
+GLOBAL_DEST_ASYNC="/usr/local/share/zsh/site-functions/$PROMPT_ASYNC"
+USER_DEST_ASYNC="${ZDOTDIR:-$HOME}/.zfunctions/$PROMPT_ASYNC"
+
+# ------------------------------------------------------------------------------
+# HELPERS
+# Useful functions for common tasks
+# ------------------------------------------------------------------------------
+
+# Paint text in specific color with reset
+# USAGE:
+#   paint <color> [text...]
+paint() {
+  local color=$1 rest=${@:2}
+  echo "$color$rest$reset"
+}
+
+# Aliases for common used colors
+# Colon at the end is required: https://askubuntu.com/a/521942
+# USAGE:
+#   info|warn|error|success|code [...text]
+info()    { paint "$cyan"   "typewritten: $@" ; }
+warn()    { paint "$yellow" "typewritten: $@" ; }
+error()   { paint "$red"    "typewritten: $@" ; }
+success() { paint "$green"  "typewritten: $@" ; }
+code()    { paint "$bold"   "typewritten: $@" ; }
+
+# Check if symlink is exists and remove it
+# USAGE:
+#   rmln <target>
+rmln() {
+  local target=$1
+  if [[ -L "$target" ]]; then
+    info "Removing $target..."
+    rm -f "$target"
+  fi
+}
+
+# ------------------------------------------------------------------------------
+# MAIN
+# Checkings and uninstalling process
+# ------------------------------------------------------------------------------
+
+remove_zshrc_content() {
+  info "Removing typewritten from \"${ZDOTDIR:-$HOME}/.zshrc\""
+  # Remove enabling statements from .zshrc
+  # and remove typewritten configuration
+  sed '/^# Set typewritten ZSH as a prompt$/d' "$ZSHRC" | \
+  sed '/^autoload -U promptinit; promptinit$/d' | \
+  sed '/^prompt typewritten$/d' | \
+  sed '/.*TYPEWRITTEN_.*=.*$/d' > "$ZSHRC.bak" && \
+  mv -- "$ZSHRC.bak" "$ZSHRC"
+}
+
+main() {
+  # Remove prompt setup symlink
+  if [[ -L "$GLOBAL_DEST_SETUP" || -L "$USER_DEST_SETUP" ]]; then
+    rmln "$GLOBAL_DEST_SETUP"
+    rmln "$USER_DEST_SETUP"
+  else
+    warn "Symlinks to typewritten setup are not found."
+  fi
+
+  # Remove prompt async symlink
+  if [[ -L "$GLOBAL_DEST_ASYNC" || -L "$USER_DEST_ASYNC" ]]; then
+    rmln "$GLOBAL_DEST_ASYNC"
+    rmln "$USER_DEST_ASYNC"
+  else
+    warn "Symlinks to typewritten async are not found."
+  fi
+
+  # Remove typewritten from .zshrc
+  if grep -q "typewritten" "$ZSHRC"; then
+    if [[ '-y' == $1 ]]; then
+      remove_zshrc_content
+    else
+      read "answer?Would you like to remove you typewritten ZSH configuration from .zshrc? (y/N)"
+      if [[ 'y' == ${answer:l} ]]; then
+        read "answer?Are you sure? Any symlinks to your ZSH configuration file might be removed? (y/N)"
+        if [[ 'y' == ${answer:l} ]]; then
+          remove_zshrc_content
+        fi
+      fi
+    fi
+  else
+    warn "typewritten configuration not found in \"${ZDOTDIR:-$HOME}/.zshrc\"!"
+  fi
+
+  success "Done! typewritten installation has been removed!"
+  success "Please, reload your terminal."
+}
+
+main "$@"
